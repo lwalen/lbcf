8c581730edc60d23c1819292c54e2231faac82c6
Lars Walen <lars@walen.me>
2016-03-06 15:43:11 -0800

    Parsing everything into a hash structure

--- parse.rb
+++ parse.rb
@@ -3,6 +3,9 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'yaml'
+
+lbcf = {}
 
 basepath = 'https://www.ccel.org/creeds/bcf/'
 main_page = 'bcf.htm'
@@ -13,15 +16,42 @@ links = page.css('a[href*="bcfc"]')
 
 links.each do |link|
   url = "#{basepath}#{link['href']}"
-  puts url
-
   page = Nokogiri::HTML(open(url))
-  chapter = page.css('a[name*="chapter"]').attr('name').to_s.gsub('chapter', '')
 
+  chapter = page.css('a[name*="chapter"]').attr('name').to_s.gsub('chapter', '')
   title = page.css('h1,h2').text.gsub('.', '')
 
-  puts "chapter: #{chapter}"
-  puts "title: #{title}"
-  puts ""
-  # exit
+  lbcf[chapter] = {title: title, paragraphs: {}}
+
+  page.css("a[name*='c#{chapter}']").each do |node|
+    paragraph = node.attr('name').gsub("c#{chapter}.", '')
+    content = node.parent
+
+    lbcf[chapter][:paragraphs][paragraph] = []
+
+    references = content.css('a[href*="scric"]')
+
+    content.children.each do |child|
+
+      if child.element? && child.css('a').any? && child.css('a').attr('href')
+
+        ref_url = child.css('a').attr('href')
+        ref_verses = page.css("a[href='#{ref_url}']").last
+
+        ref_verses.css('i').remove
+        verses = ref_verses.text.to_s.gsub("\r\n", ' ').strip
+
+        child = "<span class=\"ref\">#{verses}</span>"
+
+      elsif child.text?
+        child = child.text.to_s.gsub("#{paragraph}.", '')
+      else
+        next
+      end
+
+      lbcf[chapter][:paragraphs][paragraph] << child.to_s.gsub("\r\n", ' ').rstrip
+    end
+  end
 end
+
+puts lbcf.to_yaml
