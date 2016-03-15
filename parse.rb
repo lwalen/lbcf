b417b01d66a182451e3a076f864b6d675af23366
Lars Walen <lars@walen.me>
2016-03-14 21:16:47 -0700

    Output header and footer, put verses in data attribute

--- parse.rb
+++ parse.rb
@@ -19,7 +19,7 @@ links.each do |link|
   page = Nokogiri::HTML(open(url))
 
   chapter = page.css('a[name*="chapter"]').attr('name').to_s.gsub('chapter', '')
-  title = page.css('h1,h2').text.gsub('.', '')
+  title = page.css('h1,h2').text.gsub('.', '').gsub("\r\n", ' ')
 
   lbcf[chapter] = {title: title, paragraphs: {}}
 
@@ -31,9 +31,10 @@ links.each do |link|
 
     references = content.css('a[href*="scric"]')
 
-    content.children.each do |child|
+    content.children.each_with_index do |child, index|
 
       if child.element? && child.css('a').any? && child.css('a').attr('href')
+
 
         ref_url = child.css('a').attr('href')
         ref_verses = page.css("a[href='#{ref_url}']").last
@@ -41,7 +42,8 @@ links.each do |link|
         ref_verses.css('i').remove
         verses = ref_verses.text.to_s.gsub("\r\n", ' ').strip
 
-        child = "<span class=\"ref\">#{verses}</span>"
+        lbcf[chapter][:paragraphs][paragraph][index - 1] = "<span class=\"ref\" data-verses=\"#{verses}\">#{lbcf[chapter][:paragraphs][paragraph][index - 1]}"
+        child = "</span>"
 
       elsif child.text?
         child = child.text.to_s.gsub("#{paragraph}.", '')
@@ -54,14 +56,37 @@ links.each do |link|
   end
 end
 
+puts <<-EOS
+<html>
+<head>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="/annotate.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Martel:300' rel='stylesheet' type='text/css'>
+	<link href='/style.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="main">
+		<h1>London Baptist Confession of Faith (1689)</h1>
+EOS
+
 lbcf.each do |chapter, content|
-  puts '<section>'
-  puts "\t<h3 id=\"chapter-#{chapter}\">Chapter #{chapter}</h3>"
-  puts "\t<h2>#{content[:title]}</h2>"
+  puts "\t\t<section>"
+  puts "\t\t\t<h3 id=\"chapter-#{chapter}\">Chapter #{chapter}</h3>"
+  puts "\t\t\t<h2>#{content[:title]}</h2>"
   content[:paragraphs].each do |p_number, p_contents|
-    puts "\t<p>"
-    puts "\t\t#{p_contents.join.strip.gsub('&', '&amp;')}"
-    puts "\t</p>"
+    puts "\t\t\t<p>"
+    puts "\t\t\t\t#{p_contents.join.strip.gsub('&', '&amp;')}"
+    puts "\t\t\t</p>"
   end
-  puts '</section>'
+  puts "\t\t</section>"
 end
+
+puts <<-EOS
+	</div>
+
+	<div class="side">
+		lots of stuff goes here
+	</div>
+</body>
+</html>
+EOS
