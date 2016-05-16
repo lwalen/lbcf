4e664984c25da7d8068dad6b205748951b27bde2
Lars Walen <lars@walen.me>
2016-05-15 17:09:28 -0700

    Added rough verse map, show verse text in side bar

--- annotate.js
+++ annotate.js
@@ -1,7 +1,24 @@
+var verses = {};
 $(function() {
+  $.getJSON('/verses.json', function(data) {
+    verses = data;
+  });
+
   $('.ref').on('mouseover', function() {
     $(this).css('background-color', '#dddddd');
-    $('.side').text($(this).data('verses'));
+    var refs = $(this).data('verses').split(';');
+    var content = "";
+
+    refs.forEach(function(ref) {
+      ref = ref.trim();
+
+      if (verses[ref]) {
+        content += "<h4>" + ref + "</h4>";
+        content += "<p>" + verses[ref] + "</p>";
+      }
+    });
+
+    $('.side').html(content);
   });
 
   $('.ref').on('mouseout', function() {
